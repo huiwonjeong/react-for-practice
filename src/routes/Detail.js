d0ebf0b68ab797ce58cc4b02f822d9c999315632
huiwonjeong <hjeong303@gatech.edu>
2022-03-13 06:12:36 -0700

    update react beginnig

---
+++ src/routes/Detail.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Movie from "../components/Movie";
+
+function Detail() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(getMovie, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <Movie
+          key={movie.id}
+          coverImg={movie.medium_cover_image}
+          title={movie.title}
+          summary={movie.description_full}
+          genres={movie.genres}
+          id={movie.id}
+        />
+      )}
+    </div>
+  );
+}
+
+export default Detail;
